701ece413dff16ae042b25c639fca9fe350fdd07
Prabir Shrestha <mail@prabir.me>
2010-06-29 23:09:49 +0700

    + FacebookSettings

---
+++ src/LinqToFacebook/FacebookSettings.cs
@@ -0,0 +1,10 @@
+namespace LinqToFacebook
+{
+    public class FacebookSettings
+    {
+        public string AccessToken { get; set; }
+        public string ClientID { get; set; }
+        public string ClientSecret { get; set; }
+        public string RedirectUri { get; set; }
+    }
+}
