e96eef651665ef1ff4a04383abdae26000db1baa
Prabir Shrestha <prabir@softverk.is>
2010-06-30 10:26:09 +0700

    + broke up FacebookContext.cs to partial classes for better source code organization

---
+++ src/LinqToFacebook/FacebookContext/FacebookContext.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+
+namespace LinqToFacebook
+{
+    public partial class FacebookContext
+    {
+        private FacebookSettings _facebookSettings;
+        public FacebookSettings Settings
+        {
+            [DebuggerStepThrough]
+            get
+            {   // this assures that Facebook Settings is never null.
+                return _facebookSettings ?? (_facebookSettings = new FacebookSettings());
+            }
+            [DebuggerStepThrough]
+            set { _facebookSettings = value; }
+        }
+
+        public FacebookContext()
+        {
+            Settings = new FacebookSettings();
+        }
+
+        public FacebookContext(FacebookSettings facebookSettings)
+        {
+            Settings = facebookSettings;
+        }
+    }
+}
